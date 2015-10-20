10560e3fee8b930535841599c2e163bb8cf037e2
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2015-10-20 11:21:21 -0500

    Commit

--- Queue_Example_10202015/Program.cs
+++ Queue_Example_10202015/Program.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace Queue_Example_10202015
 {
@@ -14,6 +15,27 @@ namespace Queue_Example_10202015
     {
         static void Main(string[] args)
         {
+            Queue queue1 = new Queue();
+            queue1.Enqueue(1);
+            queue1.Enqueue(3);
+            queue1.Enqueue(5);
+            queue1.Enqueue(7);
+            queue1.Enqueue(9);
+
+            QueueMethod(queue1);
+
+            Console.ReadKey();
         }
+
+        static void QueueMethod(Queue methQueue)
+        {
+            while (methQueue.Count > 0)
+            {
+                Console.WriteLine("Now serving customer number: ");
+                Console.WriteLine(methQueue.Dequeue());
+            }
+        }
+        
     }
 }
+
